164f1097af9d915519dc0d7936d022f50900d493
upsea <upsea@upsea.cn>
2016-07-01 14:42:52 +0800

    mids upsea,xpower all reconstructed,data manager tuned for py3 and py2.

--- pull.py
+++ pull.py
@@ -3,19 +3,21 @@ import re
 import os,sys
 import subprocess
 import platform;
-import logging
+import logbook  
+logbook.StderrHandler().push_application()
+logger = logbook.Logger('push info ----')
 
 if __name__ == '__main__':
     project_dir = os.path.abspath(os.path.dirname(__file__))
     try:
         os.chdir(project_dir)
-    except Exception, e:
+    except:
         raise e       
     
     try:
         os.system('git pull git@github.com:upsea/midProjects')        
-        print 'pull oked.'
-    except Exception, error:
-        print error
+        logger.info( 'pull oked.')
+    except:
+        logger.info( e )
         
         
