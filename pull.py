49a824b7a1f3cf5e30e3c7f2caab818346e76ce6
upsea <upsea@upsea.cn>
2016-07-02 09:54:31 +0800

    mids no comments.

--- pull.py
+++ pull.py
@@ -7,17 +7,28 @@ import logbook
 logbook.StderrHandler().push_application()
 logger = logbook.Logger('pull info ----')
 
+
+def pullGit(projectDir = '',gitCommand = ''):
+    logger.info('start ----')
+    try:
+        os.chdir(projectDir)
+    except:
+        logger.info(e)
+        raise e           
+    try:
+        os.system(gitCommand)        
+        logger.info('%s Oked.' % (gitCommand))
+    except:
+        logger.info(e)    
+        raise e
+    logger.info('end #####')
+
+
 if __name__ == '__main__':
-    project_dir = os.path.abspath(os.path.dirname(__file__))
-    try:
-        os.chdir(project_dir)
-    except:
-        raise e       
+    projectDir = os.path.abspath(os.path.dirname(__file__))
+    gitCommand = 'git pull git@github.com:upsea/midProjects'
     
-    try:
-        os.system('git pull git@github.com:upsea/midProjects')        
-        logger.info( 'pull oked.')
-    except:
-        logger.info( e )
+    pullGit(projectDir=projectDir,gitCommand = gitCommand)
+
         
         
